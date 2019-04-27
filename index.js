e3f5219f1ca767b999655c3d2c64bb02f35977f3
proactive-point-1823 <daylanberry@gmail.com>
2019-04-27 03:25:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// add solution here
+function theBeatlesPlay(musicians, instrument){
+  var array = [];
+  
+  for (var i = 0; i < musicians.length; i++){
+    array.push(`${musicians} plays the ${instrument}`)
+  }
+  return array
+}
+
+function johnLennonFacts(factsArray){
+  var array = []
+  var i = 0;
+  while (array.length < factsArray){
+    array.push(factsArray[i] + '!')
+    i++;
+  }
+  return array;
+}
